5878505d79ca864033c68486c61f56720409a854
kajsawiola <kajsam@hotmail.com>
2023-02-23 13:29:53 +0100

    Lagt till 4 länder

--- Program.cs
+++ Program.cs
@@ -14,6 +14,12 @@
             Land tyskland = new Land { namn = "Tyskland", styrestyp = "republik", huvudstad = "Berlin", invånartal = "83783902" };
             Land sanMarino = new Land { namn = "SanMarino", styrestyp = "republik", huvudstad = "SanMarino", invånartal = "33600" };
             sverige.Print();
+
+            Land[] lander = new Land[7] { sverige, tyskland, sanMarino, null, null, null, null };
+            lander[4] = new Land { namn = "Danmark", styrestyp = "monarki", huvudstad = "Köpenhamn", invånartal = "5928364" };
+            lander[4] = new Land { namn = "Italien", styrestyp = "republik", huvudstad = "Rom", invånartal = "58853482" };
+            lander[4] = new Land { namn = "Danmark", styrestyp = "republik", huvudstad = "Prag", invånartal = "10551219" };
+            lander[4] = new Land { namn = "Danmark", styrestyp = "republik", huvudstad = "Bukarest", invånartal = "19760314" };
         }
     }
 }
