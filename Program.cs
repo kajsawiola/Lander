e69e88d73f56eeb72ebd6943b5a6b5d8577ccc72
kajsawiola <kajsam@hotmail.com>
2023-02-23 12:26:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Dag_14_ovn_8
+{
+    internal class Program
+    { }
+    class Land
+    {
+        public string namn, styrestyp, huvudstad, invånartal;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
