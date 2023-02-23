24dd18103d2d244e5c22283df00f238adb0f34c2
kajsawiola <kajsam@hotmail.com>
2023-02-23 13:47:21 +0100

    Lagt till forachloop

--- Program.cs
+++ Program.cs
@@ -16,10 +16,18 @@
             sverige.Print();
 
             Land[] lander = new Land[7] { sverige, tyskland, sanMarino, null, null, null, null };
-            lander[4] = new Land { namn = "Danmark", styrestyp = "monarki", huvudstad = "Köpenhamn", invånartal = "5928364" };
+            lander[3] = new Land { namn = "Danmark", styrestyp = "monarki", huvudstad = "Köpenhamn", invånartal = "5928364" };
             lander[4] = new Land { namn = "Italien", styrestyp = "republik", huvudstad = "Rom", invånartal = "58853482" };
-            lander[4] = new Land { namn = "Danmark", styrestyp = "republik", huvudstad = "Prag", invånartal = "10551219" };
-            lander[4] = new Land { namn = "Danmark", styrestyp = "republik", huvudstad = "Bukarest", invånartal = "19760314" };
+            lander[5] = new Land { namn = "Danmark", styrestyp = "republik", huvudstad = "Prag", invånartal = "10551219" };
+            lander[6] = new Land { namn = "Danmark", styrestyp = "republik", huvudstad = "Bukarest", invånartal = "19760314" };
+
+            foreach (Land i in lander)
+           
+            {
+                i.Print();
+                
+            }
         }
+
     }
 }
