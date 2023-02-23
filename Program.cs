913d4993d40c412874527601436588c7d37d3146
kajsawiola <kajsam@hotmail.com>
2023-02-23 12:33:05 +0100

    Lagt till 3 länder

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Land sverige = new Land { namn = "Sverige", styrestyp = "monarki", huvudstad = "Stockholm", invånartal = "10512820" };
+            Land tyskland = new Land { namn = "Tyskland", styrestyp = "republik", huvudstad = "Berlin", invånartal = "83783902" };
+            Land sanMarino = new Land { namn = "SanMarino", styrestyp = "republik", huvudstad = "SanMarino", invånartal = "33600" };
         }
     }
 }
